c186140a097e0fe013d94997c9a8776819a06cc9
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-04-14 19:37:51 +0530

    Loader Completed

---
+++ script.js
@@ -0,0 +1,29 @@
+var tl = gsap.timeline();
+var h5 = document.querySelector("#l1t1 h5");
+var t = 0;
+
+setInterval(function(){
+    if(t<100){
+        t++;
+        h5.innerHTML = t++;
+    }else {
+        h5.innerHTML = t;
+    }
+},50)
+
+tl.from("#l1 h1, #l1 h2",{
+    y:150,
+    stagger:0.2,
+    duration: 0.6,
+})
+tl.to("#loader",{
+    opacity:0,
+    duration: 0.4,
+    delay:2,
+    display:"none"
+})
+tl.from("#page1",{
+    y:1200,
+    opacity:0,
+    delay:0.2
+})
